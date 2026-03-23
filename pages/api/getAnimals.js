8ad5d6e736db555072ec0f31e907b603b6949387
kuzya <anteronium3001@gmail.com>
2026-03-23 04:58:27 +0300

    Use absolute path to animals.json in API route for Vercel

--- pages/api/getAnimals.js
+++ pages/api/getAnimals.js
@@ -1,8 +1,10 @@
 import fs from 'fs'
+import path from 'path'
 
 export default (req, res) => {
     try {
-        const fileContent = fs.readFileSync('public/animals.json');
+        const filePath = path.join(process.cwd(), 'public', 'animals.json');
+        const fileContent = fs.readFileSync(filePath);
         res.status(200).send(fileContent);
     }
     catch (e) {
